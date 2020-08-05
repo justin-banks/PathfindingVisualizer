10cf7e8f0c1e012f5be8db85e2fffd3442dbb828
Justin Banks <justinbanks690@gmail.com>
2020-08-05 15:00:09 -0500

    Made the grid cells toggle colors when clicked. Altered the grid so the border is not as spaced out in the middle.

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Cell.css";
 
-function Cell() {
-	return (
-		<div className="Cell">
-			<input type="button" className="CellButton" />
-		</div>
-	);
+class Cell extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			bgColor: "red",
+		};
+	}
+
+	handleClick() {
+		this.setState({
+			bgColor: this.state.bgColor == "red" ? "blue" : "red",
+		});
+	}
+	render() {
+		return (
+			<div className="Cell">
+				<input
+					type="button"
+					className="CellButton"
+					onClick={() => this.handleClick()}
+					style={{ background: this.state.bgColor }}
+				/>
+			</div>
+		);
+	}
 }
 
 export default Cell;
