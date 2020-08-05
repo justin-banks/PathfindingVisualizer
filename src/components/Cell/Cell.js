47c2dd13ad169ff6dcaf604888e43facc674c9b6
Justin Banks <justinbanks690@gmail.com>
2020-08-05 14:26:21 -0500

    created component Cell to be used in grid

---
+++ src/components/Cell/Cell.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Cell.css";
+
+function Cell() {
+	return (
+		<div className="Cell">
+			<input type="button" className="CellButton" />
+		</div>
+	);
+}
+
+export default Cell;
