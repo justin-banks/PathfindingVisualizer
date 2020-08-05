d8f3afadc3b7dc5191367d3473da97b3fb2c79d3
Justin Banks <justinbanks690@gmail.com>
2020-08-05 16:41:33 -0500

    made it so my sass variables can now be imported into my javascript files, the variables which the javascript file would have access to are contained inside of ./src/utils/_variables.scss inside of the :export statement

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import "./Cell.css";
-
+import styles from "../../utils/_variables.scss";
 class Cell extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			bgColor: "red",
+			bgColor: styles.mainColor,
 		};
 	}
 
 	handleClick() {
 		this.setState({
-			bgColor: this.state.bgColor == "red" ? "blue" : "red",
+			bgColor:
+				this.state.bgColor === styles.mainColor
+					? styles.secondaryColor
+					: styles.mainColor,
 		});
 	}
 	render() {
